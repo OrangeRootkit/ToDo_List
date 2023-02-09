44d584e9277905410bfb4ee268da268e32c6a419
OrangeRootkit <katkovdv88@gmail.com>
2023-02-09 21:11:11 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -8,150 +8,110 @@ const delete_block = document.querySelector('.delete-block');
 const listTask = document.querySelector('ul');
 const delete_complete = document.querySelector('.delete-complete');
 const delete_all = document.querySelector('.delete-all');
-const buildTask = ''
+const change = document.querySelector('.change');
 
+let arr = [];
+
+load();
+
+function hideDeleteTable() {
+    arr.length==0? delete_block.style.display = 'none':delete_block.style.display = 'flex';
+}
+hideDeleteTable();
+
+function createObj() {
+    const obj = {
+        id: new Date().toISOString(),
+        text: input.value,
+        isDone: false,  
+    };
+    return obj
+    };
+
+function creatArr () {
+    arr.push(createObj());
+    return arr;
+}
 
 //create task
-const createTask = () => {
-    let task = document.createElement('li');
-    task.classList.add("task-block");
-
-    task.innerHTML = 
-        `<input class="toggle-checkbox" type="checkbox">
-        <div class="toggle__wrapper">
-            <div class="toggle-text">${input.value}
-            </div>
-            <img class="toggle-x" src="./images/x.svg" alt="x" srcset="">
-        </div>`
+function createTask(obj) {
+        const task = document.createElement('li');
+        task.classList.add("task-block");
+        task.id = obj.id;
+        task.innerHTML =
+            `<input class="toggle-checkbox" type="checkbox" ${obj.isDone == true? "checked":""}>
+            <div class="toggle__wrapper">
+            <span class= ${obj.isDone == true? "toggle-text-checked":"toggle-text"}>${obj.text}
+            </span>
+            <img class="toggle-x" src="./images/trash.svg" alt="x" srcset="">
+        </div>`;
     return task;
-};
+}
 
-// add task to task list
-button.addEventListener('click',  ()=> {
-    if (input.value == '') {
-        input.style.backgroundColor = "rgba(255, 0, 0, 0.4)";
-        setTimeout(()=>input.style.backgroundColor ='#FFF', 1000);
-    } else {
-        listTask.appendChild(createTask());
-        input.value = '';
+// create  on-input
+function createlistTasks () {
+    listTask.innerHTML = '';
+    creatArr().forEach((obj)=> listTask.appendChild(createTask(obj)));
+}
+
+// create  on-check
+function createlistTasksChecked () {
+    listTask.innerHTML = '';
+    arr.forEach((obj)=> listTask.appendChild(createTask(obj)));
+}
+
+// 
+card.addEventListener('click', (e)=> {
+
+    if (e.target == button) {
+        input.value != ''?
+            (createlistTasks(),input.value = '')
+            : (input.style.backgroundColor = "rgba(255, 0, 0, 0.4)",
+            setTimeout(()=>input.style.backgroundColor ='#FFF', 1000))
     }
+
+    if (e.target.className == 'toggle-checkbox') {
+        arr.forEach((el)=> {
+            if (el.id == e.target.parentElement.id && el.isDone == false){
+                el.isDone = true;
+                createlistTasksChecked ()
+            } else if (el.id == e.target.parentElement.id && el.isDone == true) {
+                el.isDone = false;
+                createlistTasksChecked ()
+            }
+        });
+    };
+
+    // delete by x
+    if (e.target.className == 'toggle-x') {
+        let newArr = arr.map((el)=>el.id);
+        arr.splice(newArr.indexOf(e.target.parentElement.parentElement.id),1);
+        createlistTasksChecked ();
+    }
+    //delete all
+    if (e.target == delete_all) {
+        arr.splice(0,arr.length);
+        createlistTasksChecked ();
+    }
+    //delete complete
+    if (e.target == delete_complete) {
+        arr = arr.filter((el)=>el.isDone == false);
+        createlistTasksChecked ();
+        console.log(arr);
+        }
+        // save changes
+    localStorage.clear();
+    save();
+
+    hideDeleteTable();
 });
 
-// delete and complete function for task
-const modifyCard = () => {
-    let arr = [];
-    card.addEventListener('click', (e)=> {
-        if (e.target.className == 'toggle-x') {
-            e.target.parentElement.parentElement.remove();
-        } 
-        if (e.target.className == 'toggle-checkbox' && e.target.checked) {
-            e.target.nextElementSibling.firstElementChild.classList.add('toggle-text-checked');
-        }
-        if (e.target.className == 'toggle-checkbox' && e.target.checked == false) {
-            e.target.nextElementSibling.firstElementChild.classList.remove('toggle-text-checked')
-        }
-    });
+function save () {
+    localStorage.setItem('key', JSON.stringify(arr))
 }
-modifyCard();
 
-// delete completed 
-const deleteCompleteTasks = () => {
-    card.addEventListener('click', (e)=> {
-        if (e.target == delete_complete) {
-            let temp = document.querySelectorAll(".task-block");
-            temp.forEach((el)=> {
-                if(el.firstChild.checked){
-                    el.remove();
-                }
-            });
-        }
-    })
+function load() {
+    console.log(JSON.parse(localStorage.getItem('key')));
+    arr = JSON.parse(localStorage.getItem('key'))
+    createlistTasksChecked ();
 }
-    deleteCompleteTasks();
-
-// delete completed and delete all
-const deleteAllTasks = () => {
-    card.addEventListener('click', (e)=> {
-        if (e.target == delete_all) {
-            listTask.innerHTML = '';
-        }
-    })
-}
-deleteAllTasks();
-
-
-// save storage
-const save = () => {
-
-    function saveStorage () {
-        {
-            const arr = [];
-            localStorage.clear();
-            for (let i=0; i<listTask.children.length; i++) {
-                if (listTask.children[i].childNodes[0].checked) {
-                arr.push(listTask.children[i].childNodes[2].childNodes[1].textContent+'toggle-text-checked');
-                } else {
-                    arr.push(listTask.children[i].childNodes[2].childNodes[1].textContent);
-                }
-            }
-            localStorage.setItem('key', JSON.stringify(arr))
-        }
-    }
-
-    //listner fo all cases
-    button.addEventListener('click', saveStorage);
-    card.addEventListener('click', (e)=> {
-        if (e.target.className == 'toggle-x' || e.target == delete_all || e.target.className == 'toggle-checkbox' || e.target == delete_complete) {
-            saveStorage();
-        }
-    });
-}
-save();
-
-// load storage
-const load = () => {
-    let arr = JSON.parse(localStorage.getItem('key'));
-
-    for (let i of arr) {
-
-        // if  checked
-        if (i.includes('toggle-text-checked')) {
-            i = i.slice(0,i.indexOf('toggle-text-checked'));
-
-            function createTaskfromStorage () {
-                let task = document.createElement('li');
-                task.classList.add("task-block");
-            
-                task.innerHTML = 
-                    `<input class="toggle-checkbox" type="checkbox" checked>
-                    <div class="toggle__wrapper">
-                        <div class="toggle-text toggle-text-checked">${i}
-                        </div>
-                        <img class="toggle-x" src="./images/x.svg" alt="x" srcset="">
-                    </div>`
-
-                listTask.appendChild(task);
-            };
-            createTaskfromStorage();
-
-            // if there is no checked
-        } else {
-            function createTaskfromStorage () {
-                let task = document.createElement('li');
-                task.classList.add("task-block");
-            
-                task.innerHTML = 
-                    `<input class="toggle-checkbox" type="checkbox">
-                    <div class="toggle__wrapper">
-                        <div class="toggle-text">${i}
-                        </div>
-                        <img class="toggle-x" src="./images/x.svg" alt="x" srcset="">
-                    </div>`
-
-                listTask.appendChild(task);
-            };
-            createTaskfromStorage();
-        }
-    }
-}
-load()
